a3050a4e4d1342b2bc4448bc74e12c48c433160c
Shawn2208 <shawn.101@hotmail.com>
2024-01-23 11:45:26 +0000

    Update index.js, package files and add .gitignore and swagger.yaml

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { handleErrors } = require('./utils/errorHandler');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 const { getCornwallLiveArticles, getCornishTimesArticles } = require('./utils/articleExtractor');
 
 const app = express();
@@ -10,6 +12,10 @@ app.get('/', (req, res) => {
     res.json('This is a news API');
 });
 
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Define the newspapers array
 const newspapers = [
